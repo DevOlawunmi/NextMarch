b2fb293143b10c7bf99e44d3071b76fffe26450d
Ola <olawunmi263@yahoo.com>
2020-03-25 05:14:54 +0000

    New Feature added

---
+++ src/test/java/co/uk/next/stepDefinitions/SearchDeptStepDefinitions.java
@@ -0,0 +1,24 @@
+package co.uk.next.stepDefinitions;
+
+import co.uk.next.pages.BasePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SearchDeptStepDefinitions extends BasePage {
+
+
+    @When("I click on a {string}")
+    public void i_click_on_a(String string) {
+
+    }
+    @Then("a list of items in {string} is displayed")
+    public void a_list_of_items_in_is_displayed(String string) {
+
+    }
+    @Then("I click on any of the results")
+    public void i_click_on_any_of_the_results() {
+
+    }
+
+}
